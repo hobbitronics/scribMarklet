5e545d0a211ba95f179c97da4af3c0426cfe5895
Michael Wilson <miguel.dw@gmail.com>
2023-10-23 13:40:02 +0930

    rm useless code

--- scrib.js
+++ scrib.js
@@ -1,50 +1,39 @@
 javascript: (function () {
-  function clk(nd) {
-    const r = document.createRange();
-    r.selectNodeContents(nd);
-    r.collapse(false);
-    const sel = window.getSelection();
-    sel.removeAllRanges();
-    sel.addRange(r);
-    const rng = r.getBoundingClientRect();
-    const x = rng.right - 5;
-    const y = rng.bottom;
-    const t = document.elementFromPoint(x, y);
-    if (t) {
-      t.click();
-      const inss = document.querySelectorAll("ins");
-      const ins = [...inss].find((el) => el.textContent.trim() === "");
-      if (ins) {
-        ins.textContent = "*";
-      }
-    }
-  }
-
   function cAndT(par) {
     const nodes = [...par.childNodes].filter(
       (n) => n.nodeType === Node.TEXT_NODE
     );
     if (nodes.length > 0) {
-      const last = nodes[nodes.length - 1];
-      clk(last);
+      const nd = nodes[nodes.length - 1];
+      const r = document.createRange();
+      r.selectNodeContents(nd);
+      r.collapse(false);
+      const sel = window.getSelection();
+      sel.removeAllRanges();
+      sel.addRange(r);
+      const rng = r.getBoundingClientRect();
+      const x = rng.right - 5;
+      const y = rng.bottom;
+      const t = document.elementFromPoint(x, y);
+      if (t) {
+        t.click();
+        const inss = document.querySelectorAll("ins");
+        const ins = [...inss].find((el) => el.textContent.trim() === "");
+        if (ins) {
+          ins.textContent = "*";
+        }
+      }
     }
   }
 
   function clkParas() {
     const div = document.getElementById("work-body");
     let ps = Array.from(div.querySelectorAll("p"));
-
-    const scrollInterval = setInterval(function () {
-      if (ps.length) {
-        const p = ps[0];
-        p.scrollIntoView();
-        cAndT(p);
-        ps = ps.slice(1);
-        p.scrollIntoView();
-      } else {
-        clearInterval(scrollInterval);
-      }
-    }, 0);
+    for (p of ps) {
+      p.scrollIntoView();
+      cAndT(p);
+      p.scrollIntoView();
+    }
   }
 
   clkParas();
